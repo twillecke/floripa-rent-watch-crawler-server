d25a1f6c764b82345fc03f9b04e85e39a3a01c1f
Gbosa <gbarbosa1407@gmail.com>
2023-05-13 23:45:58 +0000

    Add DB storage feature for items and stats through pipelines. Corrected dependecies to remove shub usage.

--- zapimoveis/zapimoveis/items.py
+++ zapimoveis/zapimoveis/items.py
@@ -1,8 +1,3 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
 import scrapy
 from itemloaders.processors import TakeFirst, MapCompose
 import re
@@ -10,7 +5,7 @@ import re
 def get_address(value):
     x = value.strip().split(', ')
     if x[-1] == "Florianópolis":
-        return None
+        return "None"
     else:
         return x[0]
 
@@ -22,17 +17,17 @@ def get_region(value):
         return x[-1]
 
 def price_convert(value):
-    if value:
+    if value and value != "None":
         x = value.replace('R$ ','').replace('.','')
         return int(x)
     else:
-        return None
+        return "None"
 
 def area_convert(value):
-    if value:
+    if value and value != "None":
         return int(value.strip().split()[0])
     else:
-        return None
+        return "None"
 
 def get_housing(value):
     r = re.compile(r'[a-zA-Z]+/sc')
@@ -43,20 +38,25 @@ def get_housing(value):
         return housing_type[:-3]
 
 def strip_(value):
-    if value:
-        return value.strip()
+    if value and value != "None":
+        return int(value.strip())
     else:
-        return None
+        return "None"
 
+def rent_type_convert(value):
+    if 'dia' in value:
+        return 'D'
+    else:
+        return 'M'
 
 class ZapItem(scrapy.Item):
 
     address = scrapy.Field(input_processor=MapCompose(get_address), output_processor=TakeFirst())
     region = scrapy.Field(input_processor=MapCompose(get_region), output_processor=TakeFirst())
     housing_type = scrapy.Field(input_processor=MapCompose(get_housing), output_processor=TakeFirst())
-    rent_type = scrapy.Field(output_processor=TakeFirst())
+    rent_type = scrapy.Field(input_processor=MapCompose(rent_type_convert),output_processor=TakeFirst())
     price = scrapy.Field(input_processor=MapCompose(price_convert), output_processor=TakeFirst())
-    cond_price = scrapy.Field(input_processor=MapCompose(price_convert), output_processor=TakeFirst())
+    condominium = scrapy.Field(input_processor=MapCompose(price_convert), output_processor=TakeFirst())
     iptu_price = scrapy.Field(input_processor=MapCompose(price_convert), output_processor=TakeFirst())
     size_m2 = scrapy.Field(input_processor=MapCompose(area_convert), output_processor=TakeFirst())
     bedroom_count = scrapy.Field(input_processor=MapCompose(strip_), output_processor=TakeFirst())
