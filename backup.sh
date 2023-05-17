e434c5cffb74715fddbee3816738a23d6c4187b2
Gbosa <gbarbosa1407@gmail.com>
2023-05-17 16:30:06 +0000

    Added feature to remove old backup files

--- backup.sh
+++ backup.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/bash
 
-PGPASSFILE=~/.pgpass pg_dump -U postgres -h localhost -p 5432 -d rent_watch -f ~/backupdb/"backup_$(date +"%d%m%Y").sql"
+PGPASSFILE=~/.pgpass pg_dump -U postgres -h localhost -p 5432 -d rent_watch -f ~/backup/"backup_$(date +"%H:%M_%d%m%Y").sql" 
+
+find /home/gbarbosa1407/backup/ \( -name '*.sql' \) -print0 | sort -zr | sed -nz '3,$p' | xargs -0  rm -f
