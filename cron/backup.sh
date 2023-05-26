be10e78a512bc15712e2f52766b83b96133b1ac7
Gbosa <gbarbosa1407@gmail.com>
2023-05-26 17:05:10 +0000

    Corrected backup.sh

--- cron/backup.sh
+++ cron/backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-path="/home/project_files/backup/"
+path="$HOME/backup/"
 
 filename=backup.log
 log=$path$filename
@@ -8,11 +8,13 @@ days=13
 
 mkdir -p $path
 
+echo '' > $log 
+
 echo -e "\n------- DAY $(date +%d/%m/%Y) ------\n" >> $log
 
 echo -e "Deleting old backups:\n" >> $log
 
-if find $path -name '*.sql' -type f -mtime +$days -print -quit | grep -q . ; then
+if find $HOME/backup -name '*.sql' -type f -mtime +$days -print -quit | grep -q . ; then
     find $path -name '*.sql' -type f -mtime +$days -exec rm -f {} + -print >> $log
 else
     echo "Sorry, we didnt find any old backups." >> $log
@@ -20,6 +22,6 @@ fi
 
 echo -e "\nInitializing Backup -- $(date +%H:%M:%S)\n" >> $log
 
-PGPASSFILE=/home/project_files/.pgpass pg_dump -U postgres -h localhost -p 5432 -d rent_watch -v -f $path/backup_$(date +'%H:%M_%d%m%Y').sql 2>> $log
+PGPASSFILE=~/.pgpass pg_dump -U postgres -h localhost -p 5432 -d rent_watch -v -f ~/backup/backup_$(date +'%H:%M_%d%m%Y').sql 2>> $log
 
 echo -e "\nFinalizing Backup -- $(date +%H:%M:%S)" >> $log
