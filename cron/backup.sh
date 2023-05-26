936a728a5126cfd37bc415130a21a5b7a87bf84c
Gbosa <gbarbosa1407@gmail.com>
2023-05-26 13:06:23 +0000

    Solved formatting issues to backup.log

--- cron/backup.sh
+++ cron/backup.sh
@@ -8,9 +8,9 @@ days=13
 
 mkdir -p $path
 
-echo "\n------- DAY $(date +%d/%m/%Y) ------\n" >> $log
+echo -e "\n------- DAY $(date +%d/%m/%Y) ------\n" >> $log
 
-echo "Deleting old backups:\n" >> $log
+echo -e "Deleting old backups:\n" >> $log
 
 if find $path -name '*.sql' -type f -mtime +$days -print -quit | grep -q . ; then
     find $path -name '*.sql' -type f -mtime +$days -exec rm -f {} + -print >> $log
@@ -18,8 +18,8 @@ else
     echo "Sorry, we didnt find any old backups." >> $log
 fi
 
-echo  "\nInitializing Backup -- $(date +%H:%M:%S)\n" >> $log
+echo -e "\nInitializing Backup -- $(date +%H:%M:%S)\n" >> $log
 
 PGPASSFILE=/home/project_files/.pgpass pg_dump -U postgres -h localhost -p 5432 -d rent_watch -v -f $path/backup_$(date +'%H:%M_%d%m%Y').sql 2>> $log
 
-echo  "\nFinalizing Backup -- $(date +%H:%M:%S)" >> $log
+echo -e "\nFinalizing Backup -- $(date +%H:%M:%S)" >> $log
